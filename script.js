6b26b561b1adca8f9a5ff5401f15d7f370b526b2
davidjrb <davidjberman@gmail.com>
2024-02-07 23:01:58 +0000

    commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  fetch('https://raw.githubusercontent.com/yourUsername/yourRepository/branch/yourFile.md')
+  fetch('https://raw.githubusercontent.com/davidjrb/md2html/master/README.md')
     .then(response => response.text())
     .then(text => {
       document.getElementById('markdownContent').innerHTML = marked.parse(text);
